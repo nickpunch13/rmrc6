1a9ae4488b2cef48d2053cd5d17edb7a875e9d47
Nick Panchuk <nckpunch@yahoo.com.au>
2013-09-08 11:11:50 +1000

    Index change

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,7 +24,7 @@
 
 <body>
 
-<img src="./img/yMa2IlRJOOE.jpg" alt='image' width='1350' height='250'>
+<img src="./img/z3ejz8ictSM.jpg" alt='image' width='1350' height='250'>
 
 <div id='cssmenu'>
 <ul>
@@ -35,7 +32,7 @@
    <li><a href="about.html"><span>About Us</span></a></li>
    <li class='has-sub'><a href="benefits.html"><span>Benefits of PIR & Massage</span></a>
       <ul>
-         <li><a href="benefits.html"><span>Benefits of Post - Isometric Myorelaxation</span></a></li>
+         <li><a href="benefits.html"><span>Benefits of PIR</span></a></li>
          <li class='last'><a href="why_remedial.html"><span>Types of Massage</span></a></li>
       </ul>
    </li>
@@ -50,7 +47,7 @@
    <li class='has-sub last'><a href="faq.html"><span>FAQs & Downloads</span></a>
       <ul>
          <li><a href="faq.html"><span>Frequently Asked Questions</span></a></li>
-         <li class='last'><a href="Historyformdownload.docx"><span>Download Health History Form</span></a></li>
+         <li class='last'><a href="Historyformdownload.docx"><span>Download Form</span></a></li>
       </ul>
    </li>
 </ul>
@@ -66,10 +63,16 @@
 			</br>
 			<h4 align="center">Why Post-Isometric Myorelaxation and Remedial Massage?</h4>
 			</br>
-            <p><font size="3">The post-isometric relaxation technique begins by placing the muscle in a stretched position. Then an isometric contraction is exerted against some level of resistance. Relaxation and then gentle stretch follow as the muscle releases. This technique was applied to tight, tender muscles that are commonly associated with musculoskeletal pain. With a combination of remedial and relaxation massage elements we can stretch  all the muscles of your body and will find the point of balance of your health. </font></p>
-            <p align="center"><a class="btn btn-primary" href="http://tompit.appointy.com">Book Your Session! &raquo;</a></p>
-        </div>
-        
+			</div>
+		<div class="container">	
+			<div class="col-lg-7" align="center">
+				<p><font size="3">The post-isometric relaxation technique begins by placing the muscle in a stretched position. Then an isometric contraction is exerted against some level of resistance. Relaxation and then gentle stretch follow as the muscle releases. This technique was applied to tight, tender muscles that are commonly associated with musculoskeletal pain. With a combination of remedial and relaxation massage elements we can stretch  all the muscles of your body and will find the point of balance of your health. </font></p>
+			</div>
+			
+			<img src="./img/i (16).jpg" alt='image'>
+		</div>
+			</br>
+			<p align="center"><a class="btn btn-primary" href="http://tompit.appointy.com">Book Your Session! &raquo;</a></p>
     </div>
 
     <hr>
@@ -83,4 +86,7 @@
 <script src="./assets/js/jquery.js"></script>
 <script src="./dist/js/bootstrap.min.js"></script>
 </body>
+<?php
+
+?>
 </html>
