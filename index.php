72165cfbb171d2f625d3c0f63b660ca757e89a89
Nick Panchuk <nckpunch@yahoo.com.au>
2013-11-27 20:59:48 +1000

    Picture and text changes

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="author" content="">
     <link rel="shortcut icon" href="./assets/ico/favicon.png">
 
-    <title>REMEDIAL MASSAGE & REHABILITATION CENTRE</title>
+    <title>REMEDIAL MASSAGE & REHABILITATION THERAPY</title>
 
 
     <link href="./dist/css/bootstrap.css" rel="stylesheet">
@@ -23,81 +23,92 @@
 </head>
 
 <body>
+
 <div id='main'>
-<div id='cssmenu1'>
-		<ul>
-		<li><a href='index.php'><span>REMEDIAL MASSAGE & REHABILITATION CLINIC</span></a></li>
-		</ul>
-		
-	</div>
-	
-	</br>
-	
-	<img src="./img/clean.jpg" alt='test' width='1100' height='300'>
-	
-	</br>
-	</br>
-	
-	<div id="nav">
-		<div id='cssmenu'>
-		<ul>
-		   <li class='active'><a href="index.php"><span>Home</span></a></li>
-		   <li><a href="about.html"><span>About Us</span></a>
+	<div id='cssmenu1'>
 			<ul>
-				<li><a href="contact.html"><span>Contact Us</span></a></li>
+				<li><a href='index.php'><span>REMEDIAL MASSAGE & REHABILITATION CLINIC</span></a></li>
 			</ul>
-			</li>
-		   <li class='has-sub'><a href="benefits.html"><span>Benefits of PIR & Massage</span></a>
-			  <ul>
-				 <li><a href="benefits.html"><span>Benefits of PIR</span></a></li>
-				 <li class='last'><a href="why_remedial.html"><span>Types of Massage</span></a></li>
-			  </ul>
-		   </li>
-		   <li class='has-sub'><a href="services.html"><span>Services and Prices</span></a>
-			  <ul>
-				 <li><a href="services.html"><span>Services</span></a></li>
-				 <li class='last'><a href="hot_deals.html"><span>Hot Deals</span></a></li>
-			  </ul>
-		   </li>
-		   <li><a href="http://tompit.appointy.com"><span>Online Booking</span></a></li>
-		   <li class='has-sub last'><a href="faq.html"><span>Resources</span></a>
-			  <ul>
-				 <li><a href="faq.html"><span>Frequently Asked Questions</span></a></li>
-				 <li class='last'><a href="Historyformdownload.docx"><span>Download Form</span></a></li>
-			  </ul>
-		   </li>
-		</ul>
+			
+		</div>
+		
+		</br>
+		
+		<img src="./img/clean.jpg" alt='test' width='1100' height='300'>
+		
+		</br>
+		</br>
+		
+		<div id="nav">
+			<div id='cssmenu'>
+			<ul>
+			   <li class='active'><a href="index.php"><span>Home</span></a></li>
+			   <li><a href="about.html"><span>About Us</span></a>
+				<ul>
+					<li><a href="contact.html"><span>Contact Us</span></a></li>
+				</ul>
+				</li>
+			   <li class='has-sub'><a href="benefits.html"><span>Benefits of PIR & Massage</span></a>
+				  <ul>
+					 <li><a href="benefits.html"><span>Benefits of PIR</span></a></li>
+					 <li class='last'><a href="why_remedial.html"><span>Types of Massage</span></a></li>
+				  </ul>
+			   </li>
+			   <li class='has-sub'><a href="services.html"><span>Services and Prices</span></a>
+				  <ul>
+					 <li><a href="services.html"><span>Services</span></a></li>
+					 <li class='last'><a href="hot_deals.html"><span>Hot Deals</span></a></li>
+				  </ul>
+			   </li>
+			   <li><a href="http://tompit.appointy.com"><span>Online Booking</span></a></li>
+			   <li class='has-sub last'><a href="faq.html"><span>Resources</span></a>
+				  <ul>
+					 <li><a href="faq.html"><span>Frequently Asked Questions</span></a></li>
+					 <li class='last'><a href="Historyformdownload.docx"><span>Download Form</span></a></li>
+				  </ul>
+			   </li>
+			</ul>
 		</div>
 	</div>
-<div class="container">
+		
+	<div class="container">
 
-    <div class="row">
-        
-        <div class="col-lg-10" align="center">
-            <h1 align="center">REMEDIAL MASSAGE & REHABILITATION CENTRE</h1>			
-			<h2 align="center">If you want to be healthy, we can help you to reach your goals.</h2>
-			</br>
-			<h4 align="center">Why Post-Isometric Myorelaxation and Remedial Massage?</h4>
-			</br>
-			</div>
-		<div class="container">	
-			<div class="col-lg-7">
-				<p><font size="3">The post-isometric relaxation technique begins by placing the muscle in a stretched position. Then an isometric contraction is exerted against some level of resistance. Relaxation and then gentle stretch follow as the muscle releases. This technique was applied to tight, tender muscles that are commonly associated with musculoskeletal pain. With a combination of remedial and relaxation massage elements we can stretch  all the muscles of your body and will find the point of balance of your health. </font></p>
+		<div class="row">
+			
+			<div class="col-lg-10" align="center">
+				<h1 align="center">REMEDIAL MASSAGE & REHABILITATION THERAPY</h1>			
+				<h2 align="center">If you want to be healthy, we can help you to reach your goals.</h2>
+				</br>
+	
+				<h4 align="center">Why Post-Isometric Myorelaxation and Remedial Massage(PIR)?</h4>
+				
+				</br>
 			</div>
 			
-			<img src="./img/i (16).jpg" alt='image'>
-		</div>
+			<div class="container">	
+				<div class="col-lg-7">
+					<p><font size="3">The post-isometric relaxation technique begins by placing the muscle in a stretched position. 
+					Then an isometric contraction is exerted against some level of resistance. Relaxation and then gentle stretch follow as the muscle releases. 
+					This technique was applied to tight, tender muscles that are commonly associated with musculoskeletal pain. 
+					With a combination of remedial and relaxation massage elements we can stretch all the muscles of your body and will find the point of balance of your health. 
+					PIR technique has been designed to treat not only the cause of the problem but also the whole body. </font></p>
+				</div>
+				
+				<img src="./img/i (16).jpg" alt='image'>
+			</div>
+			
 			</br>
 			<p align="center"><a class="btn btn-primary" href="http://tompit.appointy.com">Book Your Session! &raquo;</a>
 								<a class="btn btn-success" href="hot_deals.html">Check out our specials! &raquo;</a></p>
-    </div>
 
-    <hr>
+		</div>
 
-    <footer>
-        <p>&copy; RMRC 2013</p>
-    </footer>
-</div> <!-- /container -->
+		<hr>
+
+		<footer>
+			<p>&copy; RMRC 2013</p>
+		</footer>
+	</div> <!-- /container -->
 </div>
 
 <script src="./assets/js/jquery.js"></script>
